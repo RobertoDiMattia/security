6a7165ba2da335be1912be62b0333c5b28776f1f
robertogatsu <robertogatsu@libero.it>
2023-09-14 17:52:27 +0200

    ADDED JDBC

--- src/main/java/springBootSecurity/security/SecurityConfiguration.java
+++ src/main/java/springBootSecurity/security/SecurityConfiguration.java
@@ -50,56 +50,27 @@ public class SecurityConfiguration {
         return http.build();
     }
 }
-//        http
-//                .authorizeHttpRequests((authorize) ->
-//                    authorize
-//                            .antMatchers("/admin/**").hasRole("ADMIN")
-//                )
-//                .and().formLogin(withDefaults());
-//        return http.build();
-//    }
-//}
 
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests(authorize ->
-//                        authorize
-//                                .antMatchers("/**").hasRole("ADMIN") // Consentire l'accesso a /admin solo agli utenti con ruolo "ADMIN"
-//                )
-//                .formLogin(withDefaults()); // Configurazione predefinita per la gestione del login
-//    }
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/admin/**").hasRole("ADMIN") // Consentire l'accesso a /admin solo agli utenti con ruolo "ADMIN"
-//                .anyRequest().authenticated() // Richiedere l'autenticazione per tutte le altre richieste
-//                .and()
-//                .formLogin()
-//                .loginPage("/login") // Pagina di login personalizzata (puoi personalizzarla)
-//                .permitAll() // Consentire a tutti di accedere alla pagina di login
-//                .and()
-//                .logout()
-//                .permitAll(); // Consentire a tutti di effettuare il logout
-//    }
-
-    //    @Bean
-//    public DataSource dataSource() {
-//        return new EmbeddedDatabaseBuilder()
-//                .setType(EmbeddedDatabaseType.H2)
-//                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
-//                .build();
-//    }
+    // UTILIZZANDO JDBC
+//    @Configuration
+//    public class SecurityConfiguration {
+//        @Bean
+//        public DataSource dataSource() {
+//            return new EmbeddedDatabaseBuilder()
+//                    .setType(EmbeddedDatabaseType.H2)
+//                    .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+//                    .build();
+//        }
 //
-//    @Bean
-//    public UserDetailsManager users(DataSource dataSource) {
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("password")
-//                .roles("USER")
-//                .build();
-//        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-//        users.createUser(user);
-//        return users;
+//        @Bean
+//        public UserDetailsManager users(DataSource dataSource) {
+//            UserDetails user = User.withDefaultPasswordEncoder()
+//                    .username("user")
+//                    .password("password")
+//                    .roles("USER")
+//                    .build();
+//            JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+//            users.createUser(user);
+//            return users;
+//        }
 //    }
